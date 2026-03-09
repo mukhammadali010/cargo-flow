6a7c7bbf567340d75153c2651e0f6bfbdb4f410e
mukhammadali010 <muhammadalinosirov34@gmail.com>
2026-03-10 00:07:54 +0500

    project finished

--- src/app/features/notifications/notification/notification.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-notification',
-  imports: [],
-  templateUrl: './notification.html',
-  styles: ``,
-})
-export class Notification {}
--- src/app/shared/components/status-badge/status-badge.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-status-badge',
-  imports: [],
-  templateUrl: './status-badge.html',
-  styles: ``,
-})
-export class StatusBadge {}
